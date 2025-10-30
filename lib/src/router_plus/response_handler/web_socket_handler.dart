7fbf85ad7dbad2e60e160da334f95478bdef59be
busslina <73592852+busslina@users.noreply.github.com>
2025-10-30 04:19:20 +0100

    Upgraded shelf_web_socket dependency
    From ^2.0.0 to ^3.0.0

--- lib/src/router_plus/response_handler/web_socket_handler.dart
+++ lib/src/router_plus/response_handler/web_socket_handler.dart
@@ -1,12 +1,11 @@
 import 'package:shelf_plus/shelf_plus.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart' as shelf_web_socket;
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 ResponseHandler get webSocketHandler =>
     (Request request, dynamic maybeWebSocketSession) {
       if (maybeWebSocketSession is WebSocketSession) {
         return shelf_web_socket
-            .webSocketHandler((WebSocketChannel webSocketChannel) {
+            .webSocketHandler((webSocketChannel, subprotocol) {
           maybeWebSocketSession.init(webSocketChannel);
         });
       }
